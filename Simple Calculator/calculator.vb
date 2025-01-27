f6d61f30f1361f9649c2ff2092101ccf2bf2f2ae
Chris Zak <kristof_evo@gmail.com>
2025-01-26 20:52:23 -0700

    Add project files.

---
+++ Simple Calculator/calculator.vb
@@ -0,0 +1,25 @@
+﻿'ChristopherZ
+'Spring 2025
+'RCET2265
+'Operators Example
+'https://github.com/Christopher-isu/SayMyName.git
+
+Option Explicit On 'forces all variables to be declared
+Module calculator
+    Sub Main()
+        ' Prompt the user to enter their name
+        Console.Write("Enter your name: ")
+
+        ' Save user input as a variable
+        Dim userName As String = Console.ReadLine()
+
+        ' Display a hello message with the user's name
+        Console.WriteLine("Hello, " & userName & "!")
+
+        ' Keep console open untill button pressed
+        Console.WriteLine("Press any key to exit.")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
