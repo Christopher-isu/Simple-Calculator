a85962b27e7aed4f0c6ebdbe18b9b22d2a8dc26c
Chris Zak <kristof_evo@gmail.com>
2025-01-26 21:55:12 -0700

    added case

--- Simple Calculator/calculator.vb
+++ Simple Calculator/calculator.vb
@@ -7,14 +7,35 @@
 Option Explicit On 'forces all variables to be declared
 Module calculator
     Sub Main()
-        ' Prompt the user to enter their name
-        Console.Write("Enter your name: ")
+        ' Prompt user for the first number
+        Console.WriteLine("This is a simple calculator example that can perform simple operations on two integers.")
+        Console.WriteLine("Please enter the first number:")
 
-        ' Save user input as a variable
-        Dim userName As String = Console.ReadLine()
+        ' Save the frist number in a variable
+        Dim firstNumber As Integer = Console.ReadLine()
 
-        ' Display a hello message with the user's name
-        Console.WriteLine("Hello, " & userName & "!")
+        ' Prompt user for the first number
+        Console.WriteLine("Enter the second number:")
+
+        ' Save the second number in a variable
+        Dim secondNumber As Integer = Console.ReadLine()
+
+        ' Prompt user for the first number
+        Console.WriteLine("Choose operation:")
+        Console.WriteLine("1. Sum")
+        Console.WriteLine("2. Product")
+
+        ' Save the operation in a variable
+        Dim operation As Integer = Console.ReadLine()
+
+        Select Case operation
+            Case 1
+                Console.WriteLine("The sum is: " & (firstNumber + secondNumber))
+            Case 2
+                Console.WriteLine("The product is: " & (firstNumber * secondNumber))
+            Case Else
+                Console.WriteLine("Invalid operation.")
+        End Select
 
         ' Keep console open untill button pressed
         Console.WriteLine("Press any key to exit.")
